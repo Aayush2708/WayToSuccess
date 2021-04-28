4200832566623ae4c37d82a0519cb45aac2485b3
AAYUSH A GUPTA <aayushgupta2708@gmail.com>
2021-04-28 16:49:19 +0530

    card making done successfully

--- waytosuccess/lib/main.dart
+++ waytosuccess/lib/main.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:waytosuccess/Drawer.dart';
 void main()  {
   runApp(MaterialApp(
     home: HomePage(),
@@ -65,36 +66,7 @@ class _HomePageState extends State<HomePage> {
         ),
         ),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.all(0),
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("AAYUSH GUPTA"), 
-              accountEmail: Text("aayushgupta2708@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  "https://images.unsplash.com/photo-1588731247530-4076fc99173e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzB8fGd1eXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
-
-                ),
-                ),
-              ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("AAYUSH GUPTA"),
-              subtitle: Text("Developer"),
-              trailing: Icon(Icons.edit),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Email"),
-              subtitle: Text("aayushgupta2708@gmail.com"),
-              trailing: Icon(Icons.emoji_people),
-            ),
-          ],
-        ),
-      ),
+      drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           myText = _nameController.text ;
